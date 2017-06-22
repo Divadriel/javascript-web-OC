563622b195f03b2d248ae813134bb20da11eebec
Divadriel <rei.david26@gmail.com>
2017-06-22 15:14:16 +0200

    first chapter with display childNode exercise

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Exercices Javascript</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <script src="jsweb.js"></script>
+        <div>
+            <p>
+                Ouvrez la console pour accéder aux messages !
+            </p>
+        </div>
+        <div>
+            <h2>Accès aux divers exercices et activités</h2>
+            <ul>
+                <li>
+                    <a href="child.php">Affichage d'un noeud enfant</a>
+                </li>
+                <li>
+
+                </li>
+            </ul>
+        </div>
+    </body>
+</html>
